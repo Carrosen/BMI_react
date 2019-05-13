96a6762ccbbf0c0231ad218559d1cad1c4e29fb7
Carrosen <carla1.rosen@gmail.com>
2019-05-13 12:09:00 +0200

    implements setState, adds constructor to declare the state and default value of the states

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weight: '',
+      height: ''
+    }
+  }
+  
   render() {
     return (
       <>
+        <div>
+          <label>Weight(kg)</label>
+          <input name="weight" value={this.state.weight} onChange={ (e) => this.setState({ weight: e.target.value })} />
+        </div>
 
+        <div>
+          <label>Height</label>
+          <input name="height" value={this.state.height} onChange={ (e) => this.setState({ height: e.target.value })}/>
+        </div>
       </>
-    )
+    );
   }
 }
 
